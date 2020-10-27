b4ee1b2520aa84545892e29f0e2752d5cdb97214
bearbearbear <ghlwkdthfl@gmail.com>
2020-10-27 13:39:30 +0900

    feat: 상품 옵션 객체 정의 및 옵션을 받을 Clothes 클래스 생성

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,38 @@
-console.log("hello world!");
+// category - t-shirt, pants, skirt
+// gender - man, female
+// size - small, big
+// material - smooth, tough
+// color - blue, yellow, pink
+
+// 1. options 안에서 랜덤으로 각 옵션을 하나씩 뽑아서 
+// 2. Clothes 에 각 옵션을 인자로 부여해서
+// 3. 랜덤 데이터 생성
+
+const options = {
+    category: ["t-shirt", "pants", "skirt"],
+    gender: ["man", "female"],
+    size: ["small", "big"],
+    material: ["smooth", "tough"],
+    color: ["blue", "yellow", "pink"],
+
+    // 각 배열 길이에 맞춰 랜덤 값을 출력하는 함수
+    
+}
+
+class Clothes {
+    constructor(category, gender, size, material, color){
+        this.category = category;
+        this.gender = gender;
+        this.size = size;
+        this.material = material;
+        this.color = color;
+    }
+
+    log () {
+        console.log(this.color);
+    }
+}
+
+const clothes = new Clothes("t-shirt", "man", "big", "tough", "red");
+
+clothes.log();
