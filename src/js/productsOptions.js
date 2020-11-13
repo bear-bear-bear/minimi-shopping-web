028969f0106eef33c1126c4269f8cc24c935b661
bearbearbear <bear-bear-bear@kakao.com>
2020-11-14 04:02:14 +0900

    refactor: 스크립트 모듈화

---
+++ src/js/productsOptions.js
@@ -0,0 +1,13 @@
+// gender - man, female
+// category - t-shirt, pants, skirt
+// size - small, big
+// material - smooth, tough
+// color - blue, yellow, pink
+
+export const productsOptions = {
+  gender: ["man", "female"],
+  category: ["t-shirt", "pants", "skirt"],
+  size: ["small", "big"],
+  material: ["smooth", "tough"],
+  color: ["blue", "yellow", "pink"],
+};
