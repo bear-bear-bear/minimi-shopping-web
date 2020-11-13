189024d4b0bab109d4e7b1c3950a3db0d87a9370
bearbearbear <bear-bear-bear@kakao.com>
2020-11-14 04:05:45 +0900

    feat: 주어진 옵션으로 가상 데이터(상품) 객체를 반환하는 함수 작성
    - 현재 설정된 가상 데이터(상품) 개수 - 100개 (변경가능)

---
+++ src/js/createFakeData.js
@@ -0,0 +1,38 @@
+import { productsOptions } from "./productsOptions";
+
+const NUMBER_OF_PRODUCTS = 100;
+
+const createFakeData = (options) => {
+  let fakeData = {};
+
+  fakeData = toArray(options).map((elem) => {
+    let key = elem[0];
+    let randomElem = getRandomElem(elem[1]);
+    return [key, randomElem];
+  });
+
+  return toObject(fakeData);
+
+  function toArray(obj) {
+    return Object.entries(obj);
+  }
+
+  function toObject(arr) {
+    return Object.fromEntries(arr);
+  }
+
+  function getRandomElem(arr) {
+    let randomIndex = getRandomNum(arr.length);
+    return arr[randomIndex];
+  }
+
+  function getRandomNum(arrLength) {
+    return parseInt(Math.random() * arrLength);
+  }
+};
+
+const products = [];
+
+for (let i = 0; i < NUMBER_OF_PRODUCTS; i++) products.push(createFakeData(productsOptions));
+
+export const fakeProductsData = products; // fakeData test
--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,4 @@
-// category - t-shirt, pants, skirt
-// gender - man, female
-// size - small, big
-// material - smooth, tough
-// color - blue, yellow, pink
-
-// 1. options 안에서 랜덤으로 각 옵션을 하나씩 뽑아서 
-// 2. Clothes 에 각 옵션을 인자로 부여해서
-// 3. 랜덤 데이터 생성
-
-const options = {
-    category: ["t-shirt", "pants", "skirt"],
-    gender: ["man", "female"],
-    size: ["small", "big"],
-    material: ["smooth", "tough"],
-    color: ["blue", "yellow", "pink"],
-
-    // 각 배열 길이에 맞춰 랜덤 값을 출력하는 함수
-    
-}
+import { fakeProductsData as fakeData } from "./createFakeData";
 
 class Clothes {
     constructor(category, gender, size, material, color){
@@ -35,4 +16,6 @@ class Clothes {
 
 const clothes = new Clothes("t-shirt", "man", "big", "tough", "red");
 
-clothes.log();
+clothes.log(); // Clothes class test
+
+console.log(fakeData); // fakeData object test
