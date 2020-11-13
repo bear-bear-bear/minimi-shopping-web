7bde7fd13827707677ba8ca09d9123277585cbc1
bearbearbear <bear-bear-bear@kakao.com>
2020-11-14 04:06:36 +0900

    style: 들여쓰기 간격 변경

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,17 @@
 import { fakeProductsData as fakeData } from "./createFakeData";
 
 class Clothes {
-    constructor(category, gender, size, material, color){
-        this.category = category;
-        this.gender = gender;
-        this.size = size;
-        this.material = material;
-        this.color = color;
-    }
+  constructor(category, gender, size, material, color) {
+    this.category = category;
+    this.gender = gender;
+    this.size = size;
+    this.material = material;
+    this.color = color;
+  }
 
-    log () {
-        console.log(this.color);
-    }
+  log() {
+    console.log(this.color);
+  }
 }
 
 const clothes = new Clothes("t-shirt", "man", "big", "tough", "red");
