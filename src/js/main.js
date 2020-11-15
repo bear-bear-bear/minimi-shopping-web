546fbbe762c9e0aebd186b3824757ee97399b8c7
bearbearbear <bear-bear-bear@kakao.com>
2020-11-16 00:50:26 +0900

    feat: 서버단의 응답을 가정한 데이터 반환 함수 작성

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,17 @@
-import { fakeProductsData as fakeData } from "./createFakeData.js";
+// 서버단에서 현재 페이지 넘버에 따라 DB에서 데이터를 가져와서 응답하는 부분을 가정한 구현
+import { fakeProductsData as DB } from "./createFakeData.js"; // 가상 데이터 객체를 DataBase 라고 가정
 
-class Clothes {
-  constructor(category, gender, size, material, color) {
-    this.category = category;
-    this.gender = gender;
-    this.size = size;
-    this.material = material;
-    this.color = color;
+const getCurrentPageData = (currPage, productsNumPerPage) => {
+  let offset = currPage * productsNumPerPage - 1; // 표시할 데이터 중 첫번째 데이터의 좌표
+  let currentPageData = [];
+
+  for (let i = offset; i < offset + productsNumPerPage; i++) {
+    currentPageData.push(DB[i]);
   }
 
-  log() {
-    console.log(this.color);
-  }
-}
+  return currentPageData;
+};
 
-const clothes = new Clothes("t-shirt", "man", "big", "tough", "red");
+// 클라이언트 단에서 현재 페이지 넘버와 함께 데이터를 요청하는 부분을 가정한 구현
 
-clothes.log(); // Clothes class test
-
-console.log(fakeData); // fakeData object test
+// 클라이언트 단에서 해당 데이터를 받아 정제과정을 거친 후 화면에 출력
