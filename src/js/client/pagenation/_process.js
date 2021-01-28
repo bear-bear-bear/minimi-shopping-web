9facfc051bbab4e770f36f2d6877cb0a7edfc71f
bearbearbear <bear-bear-bear@kakao.com>
2021-01-28 19:29:52 +0900

    fix: html 공백 제거

--- src/js/client/pagenation/_process.js
+++ src/js/client/pagenation/_process.js
@@ -4,14 +4,14 @@ const IMG_DIR_PATH = './src/img/';
 // 클라이언트 단에서 표시되는 데이터의 형식
 const dataToElement = (dataObj) => {
   return `
-  <li class='app__products__item'>
-    <section class='app__products__item__image' style='background-image: url(${IMG_DIR_PATH}${dataObj.color}_${dataObj.category[0]}.png);'></section>
-    <section class='app__products__item__desc'>
-      <section class='app_products__item__name app_products__item__gender-size'>${dataObj.gender}, ${dataObj.size}</section>
-      <section>${dataObj.color} ${dataObj.material} ${dataObj.category}</section>
-    </section>
-  </li>
-  `.trim();
+<li class='app__products__item'>
+  <section class='app__products__item__image' style='background-image: url(${IMG_DIR_PATH}${dataObj.color}_${dataObj.category[0]}.png);'></section>
+  <section class='app__products__item__desc'>
+    <section class='app_products__item__name app_products__item__gender-size'>${dataObj.gender}, ${dataObj.size}</section>
+    <section>${dataObj.color} ${dataObj.material} ${dataObj.category}</section>
+  </section>
+</li>
+`.trim();
 };
 
 export default dataToElement;
